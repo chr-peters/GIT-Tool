4775466703c02b52784cda6e4f61e0ea9b197acd
cxan96 <c.p.xanten@gmail.com>
2016-11-15 10:36:19 +0100

    added gitcommandexecutor package and added target test-executor in build.xml

---
+++ src/application/git_tool/gitcommandexecutor/GITCommandExecutor.java
@@ -0,0 +1,9 @@
+package application.git_tool.gitcommandexecutor;
+
+public class GITCommandExecutor {
+
+    public static void main(String args []) {
+        System.out.println("Hello");
+    }
+
+}
