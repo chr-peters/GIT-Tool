128c6db6a98194184f221ecf86751f6d587fa0db
niklas <niklas.selke@web.de>
2016-11-16 23:53:56 +0100

    added treewillexpand listener

--- src/application/git_tool/filebrowser/FileBrowser.java
+++ src/application/git_tool/filebrowser/FileBrowser.java
@@ -5,8 +5,7 @@ import application.git_tool.GITTool;
 
 import java.awt.Component;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 import net.miginfocom.layout.*;
 import net.miginfocom.swing.*;
@@ -18,71 +17,65 @@ import javax.swing.tree.*;
 
 public class FileBrowser extends JPanel {
     private GITTool gitTool;
+    private JTree tree;
+    private JList<File> list;
     
-    private class TreeExpansionHandler implements TreeExpansionListener {
-        public void treeExpanded(TreeExpansionEvent e) {
+    private class MyTreeModel extends DefaultTreeModel {
+        private MyTreeModel(MyTreeNode node) {
+            super(node);
         }
         
-        public void treeCollapsed(TreeExpansionEvent e) {
-        }
-    }
-
-    private class TreeNode extends DefaultMutableTreeNode {
-        private File f;
-        private boolean loaded;
-
-        private TreeNode(File path) {
-            this.f = path;
-            this.loaded = false;
-            this.setUserObject(this.f.getName());
-        }
-
-        private void lazyLoad() {
-            if(!this.loaded) {
-                /*try {
-                    String[] content = this.f.list();
-                    Arrays.sort(content, String.CASE_INSENSITIVE_ORDER);
-                    ArrayList<File> files = new ArrayList<File>();
-                    int i=0;
-                    for(String s: content) {
-                        File file = new File(this.f.getAbsoluteFile()+"/"+s);
-                        if(file.isDirectory()) {
-                            this.insert(new TreeNode(file), i++);
-                        } else {
-                            files.add(file);
-                        }
-                    }
-                    for(File file: files) {
-                        this.insert(new TreeNode(file), i++);
-                    }
-                } catch(NullPointerException e) {
-                }*/
-                File tmp = FileBrowser.this.gitTool.getProcessBuilder().directory();
-                FileBrowser.this.gitTool.getProcessBuilder().directory(this.f.getAbsoluteFile());
-                File[] content = FileBrowser.this.getContent();
-                FileBrowser.this.gitTool.getProcessBuilder().directory(tmp.getAbsoluteFile());
-                for(int i=0; i<content.length; i++) {
-                    this.insert(new TreeNode(content[i]), i);
-                }
-                loaded = true;
-            }
-        }
-
         @Override
-        public boolean isLeaf() {
-            return this.f.isFile();
-        }
-
-        @Override
-        public int getChildCount() {
-            this.lazyLoad();
-            return super.getChildCount();
+        public boolean isLeaf(Object node) {
+            return ((MyTreeNode) node).path.isFile();
         }
     }
     
-    private class FileRenderer extends DefaultListCellRenderer {
+    private class MyTreeNode extends DefaultMutableTreeNode {
+        private File path;
+        private boolean loaded;
+        
+        private MyTreeNode(File path) {
+            this.path = path.getAbsoluteFile();
+            this.loaded = false;
+            this.setUserObject(this.path.getName());
+            if(path.getAbsolutePath().equals("/")) {
+                this.load();
+            }
+        }
+        
+        private void load() {
+            if(!this.loaded) {
+                File[] content = FileBrowser.this.getContent(this.path);
+                for(int i=0; i<content.length; i++) {
+                    this.insert(new MyTreeNode(content[i]), i);
+                }
+                this.loaded = true;
+            }
+        }
+    }
+    
+    private class MyTreeWillExpandListener implements TreeWillExpandListener {
         @Override
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        public void treeWillExpand(TreeExpansionEvent e) {
+            ((MyTreeNode) e.getPath().getLastPathComponent()).load();
+            FileBrowser.this.gitTool.getProcessBuilder().directory(((MyTreeNode) e.getPath().getLastPathComponent()).path);
+        }
+        
+        @Override
+        public void treeWillCollapse(TreeExpansionEvent e) {
+            MyTreeNode node = (MyTreeNode) e.getPath().getLastPathComponent();
+            if(node.getParent()!=null) {
+                FileBrowser.this.gitTool.getProcessBuilder().directory(((MyTreeNode) node.getParent()).path);
+            } else {
+                FileBrowser.this.gitTool.getProcessBuilder().directory(((MyTreeNode) node.getRoot()).path);
+            }
+        }
+    }
+    
+    private class MyListCellRenderer extends DefaultListCellRenderer {
+        @Override
+        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
             JLabel l = (JLabel) c;
             File f = (File) value;
@@ -91,36 +84,47 @@ public class FileBrowser extends JPanel {
             return l;
         }
     }
-
+    
     public FileBrowser(GITTool gitTool) {
         this.gitTool = gitTool;
+        String[] startPath = this.gitTool.getProcessBuilder().directory().getAbsolutePath().substring(1).split(File.separator);
         this.setLayout(new MigLayout());
         
-        JTree tree = new JTree(new TreeNode(new File("/")));
+        this.tree = new JTree(new MyTreeModel(new MyTreeNode(new File("/"))));
+        this.tree.addTreeWillExpandListener(new MyTreeWillExpandListener());
+        MyTreeNode startNode = (MyTreeNode) this.tree.getModel().getRoot();
+        for(String s: startPath) {
+            for(int i=0; i<startNode.getChildCount(); i++) {
+                if(((MyTreeNode) startNode.getChildAt(i)).getUserObject().equals(s)) {
+                    startNode = (MyTreeNode) startNode.getChildAt(i);
+                    this.tree.expandPath(new TreePath(startNode.getPath()));
+                    break;
+                }
+            }
+        }
         JScrollPane scrollPane = new JScrollPane();
-        scrollPane.getViewport().add(tree);
-        this.openStartPath(tree);
-        tree.addTreeExpansionListener(new TreeExpansionHandler());
+        scrollPane.getViewport().add(this.tree);
         this.add(scrollPane, "width 40%, height 100%");
+        this.tree.scrollPathToVisible(new TreePath(startNode.getPath()));
         
-        JList<File> fileList = new JList<File>(this.getContent());
+        this.list = new JList<File>(this.getContent(this.gitTool.getProcessBuilder().directory()));
+        this.list.setCellRenderer(new MyListCellRenderer());
+        this.list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        this.list.setVisibleRowCount(0);
         JScrollPane scrollPane2 = new JScrollPane();
-        scrollPane2.getViewport().add(fileList);
-        fileList.setCellRenderer(new FileRenderer());
-        fileList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-        fileList.setVisibleRowCount(0);
+        scrollPane2.getViewport().add(this.list);
         this.add(scrollPane2, "width 60%, height 100%");
     }
     
-    private File[] getContent() {
+    private File[] getContent(File path) {
         try{
-            String[] contentString = this.gitTool.getProcessBuilder().directory().list();
+            String[] contentString = path.getAbsoluteFile().list();
             Arrays.sort(contentString, String.CASE_INSENSITIVE_ORDER);
             ArrayList<File> files = new ArrayList<File>();
             File[] content = new File[contentString.length];
             int i=0;
             for(String s: contentString) {
-                File file = new File(this.gitTool.getProcessBuilder().directory().getAbsoluteFile()+"/"+s);
+                File file = new File(path.getAbsoluteFile()+"/"+s);
                 if(file.isDirectory()) {
                     content[i++] = file;
                 } else {
@@ -135,27 +139,7 @@ public class FileBrowser extends JPanel {
             return new File[] {};
         }
     }
-
-    private void openStartPath(JTree tree) {
-        if(this.gitTool.getProcessBuilder().directory().getAbsolutePath().equals("/")) {
-            return;
-        }
-        String[] pathParts = this.gitTool.getProcessBuilder().directory().getAbsolutePath().substring(1).split(File.separator);
-        TreeNode startNode = (TreeNode) tree.getModel().getRoot();
-        for(String s: pathParts) {
-            for(int i=0; i<startNode.getChildCount(); i++) {
-                if(((TreeNode) startNode.getChildAt(i)).getUserObject().equals(s)) {
-                    startNode = (TreeNode) startNode.getChildAt(i);
-                    break;
-                }
-            }
-        }
-        TreePath startPath = new TreePath(startNode.getPath());
-        tree.expandPath(startPath);
-        tree.scrollPathToVisible(startPath);
-    }
     
     public void refresh() {
-        
     }
 }
