1bfee747b8ad234381f9ae8e9a68b2cf60548b38
Marius Nolden <m.nolden@fz-juelich.de>
2016-11-17 11:04:37 +0100

    Added GitTool.java to Version Control

--- src/application/git_tool/GITTool.java
+++ src/application/git_tool/GITTool.java
@@ -14,21 +14,21 @@ public class GITTool {
     public static void main (String args []){
         JFrame frame = new JFrame("GIT Tool");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         MigLayout layout = new MigLayout("debug");
         JPanel rootContainer = new JPanel(layout);
-        
+
         FileBrowser fileBrowser = new FileBrowser(new File("."));
         CommandMenu commandMenu = new CommandMenu();
-        
+
         rootContainer.add(commandMenu, "width 25%, height 75%");
         rootContainer.add(fileBrowser, "width 50%, spany 2, growx, growy, height 100%");
         rootContainer.add(new JLabel("Info"), "width 25%, spany 3, growx, growy, wrap");
         rootContainer.add(new JLabel("History"), "spany 2, growx, growy, height 25%,wrap");
         rootContainer.add(new JLabel("Terminal"), "growx, growy, skip, skip, skip");
-        
+
         frame.getContentPane().add(rootContainer);
-        
+
         frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
