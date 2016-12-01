c7780e35c738b453839cea7c1a30587b756806e2
cxan96 <c.p.xanten@gmail.com>
2016-12-01 11:38:34 +0100

    save last commands to log file and parse them on startup

--- src/application/git_tool/history/History.java
+++ src/application/git_tool/history/History.java
@@ -10,6 +10,11 @@ import javax.swing.*;
 
 import java.awt.Color;
 
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+import java.io.*;
+
 import java.util.LinkedList;
 
 public class History extends JPanel {
@@ -20,6 +25,11 @@ public class History extends JPanel {
     //list to store the commands
     private LinkedList<Command> commands;
     
+    private final String dateFormat = "dd.MM.yyyy' - 'HH:mm:ss";
+    private final File commandLog = new File("src/data/commands.log");
+    private final String separator = ",";
+    private final String escapeSequence = "\\";
+    
     public History (GITTool gitTool, int maxCommands){
         this.gitTool = gitTool;
         this.maxCommands = maxCommands;
@@ -28,6 +38,29 @@ public class History extends JPanel {
         //init GUI
         this.setLayout(new MigLayout("fillx"));
         this.setBorder(BorderFactory.createTitledBorder("Command History"));
+        
+        //parse the commands from the log file and draw them
+        this.parseCommands();
+        this.drawCommands();
+        
+        //add shutdown routine
+        Runtime.getRuntime().addShutdownHook(new Thread (new Runnable () {
+            public void run () {
+                try {
+                    //write the last commands into the commands.log file
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(History.this.commandLog, false));
+                    for(Command c: History.this.commands) {
+                        //escape the separator sequence in case it is part of the command text or date
+                        writer.write(c.printDate(History.this.dateFormat).replace(History.this.separator, History.this.escapeSequence+History.this.separator) 
+                                        + History.this.separator + c.getText().replace(History.this.separator, History.this.escapeSequence+History.this.separator));
+                        writer.newLine();
+                    }
+                    writer.close();
+                } catch (IOException e) {
+                    System.err.println("Could not save the last commands to the log file.");
+                }
+            }
+        }));
     }
     
     private void drawCommands() {
@@ -35,7 +68,7 @@ public class History extends JPanel {
         this.removeAll();
         //draw the commands
         for(int i=0; i<commands.size(); i++){
-            this.add(new JLabel(commands.get(i).printDate("dd.MM.yyyy' - 'HH:mm:ss")), "");
+            this.add(new JLabel(commands.get(i).printDate(dateFormat)), "");
             this.add(new JLabel(commands.get(i).getText()), "wrap");
         }
         this.revalidate();
@@ -71,4 +104,28 @@ public class History extends JPanel {
         this.drawCommands();
     }
     
+    //parse the last commands from the log file
+    private void parseCommands() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(this.commandLog));
+            String line = "";
+            while ((line=reader.readLine()) != null && commands.size() < this.maxCommands){
+                try {
+                    //split the line along the unescaped separator sequence
+                    String components [] = line.split("[^"+(this.escapeSequence=="\\"?"\\":"")+this.escapeSequence+"]"+this.separator);
+                    //replace the escaped separator sequences with the standard separator sequences
+                    String dateString = components[0].replace(this.escapeSequence+this.separator, this.separator);
+                    String textString = components[1].replace(this.escapeSequence+this.separator, this.separator);
+                    //parse the date format
+                    SimpleDateFormat dateParser = new SimpleDateFormat(this.dateFormat);
+                    this.commands.add(new Command(textString, dateParser.parse(dateString)));
+                } catch (Exception e) {
+                    System.err.println("Could not read line from "+this.commandLog.toString()+". Invalid command!");
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+
+        }
+    }
 }
