bdf109ef3e29fc399dfec5016d22dd71511bf5ab
cxan96 <c.p.xanten@gmail.com>
2016-12-06 11:54:34 +0100

    solved border problem

--- src/application/git_tool/commandline/CommandLine.java
+++ src/application/git_tool/commandline/CommandLine.java
@@ -8,6 +8,7 @@ import net.miginfocom.swing.*;
 
 import javax.swing.*;
 import javax.swing.text.*;
+import javax.swing.border.*;
 import java.awt.Color;
 
 public class CommandLine extends JPanel {
@@ -22,15 +23,29 @@ public class CommandLine extends JPanel {
     public CommandLine (GITTool gitTool){
         this.gitTool = gitTool;
         
-        this.setLayout(new MigLayout("debug, fillx, filly"));
+        this.setLayout(new MigLayout("gap rel 0, fillx, filly"));
+        
+        //initialize the output field for the terminal
         this.outputPane = new JTextPane();
+        this.outputPane.setBorder(BorderFactory.createEmptyBorder());
         this.outputDoc = outputPane.getStyledDocument();
+        //set the style of the ouput field
         Style style = this.outputPane.addStyle("style", null);
         this.outputPane.setBackground(Color.BLACK);
         StyleConstants.setForeground(style, Color.GREEN);
         this.outputPane.setEditable(false);
         
-        this.add(new JScrollPane(outputPane), "width 100%, growy");
+        //initialize the input field for the terminal
+        this.inputField = new JTextField();
+        this.inputField.setBorder(BorderFactory.createEmptyBorder());
+        this.inputField.setBackground(Color.BLACK);
+        this.inputField.setForeground(Color.GREEN);
+        this.inputField.setCaretColor(Color.GREEN);
+        
+        JScrollPane outPutPaneScroll = new JScrollPane(outputPane);
+        outPutPaneScroll.setBorder(BorderFactory.createEmptyBorder());
+        this.add(outPutPaneScroll, "width 100%, growy, pushy,  wrap");
+        this.add(inputField, "height pref, growx");
     }
     
     public void refresh () {
