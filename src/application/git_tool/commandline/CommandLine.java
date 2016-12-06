f43ea40fadf716c3a5ebe466329c751a652abc91
cxan96 <c.p.xanten@gmail.com>
2016-12-06 15:07:50 +0100

    terminal starts in the current directory

--- src/application/git_tool/commandline/CommandLine.java
+++ src/application/git_tool/commandline/CommandLine.java
@@ -34,7 +34,7 @@ public class CommandLine extends JPanel {
         this.gitTool = gitTool;
         this.processBuilder = new ProcessBuilder();
         this.processBuilder.redirectErrorStream(true);
-        this.processBuilder.directory(new File("."));
+        this.processBuilder.directory(gitTool.getProcessBuilder().directory());
         this.commandExecutor = new CommandExecutor(this.processBuilder);
         
         this.setLayout(new MigLayout("gap 0, insets 0, fillx, filly"));
